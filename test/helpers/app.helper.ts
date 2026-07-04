57d5440723715d5178482871a1eefeeb12beb342
Harry Vasquez <hjvasquez90@gmail.com>
2026-07-04 03:05:29 -0500

    fix: use driver.reset() for deterministic app state in CI

--- test/helpers/app.helper.ts
+++ test/helpers/app.helper.ts
@@ -1,7 +1,3 @@
 export async function resetApp() {
-    const appId = "io.appium.android.apis"
-    await driver.terminateApp(appId)
-    await browser.pause(2000)
-    await driver.activateApp(appId)
-    await browser.pause(3000)
+    await driver.reset()
 }
